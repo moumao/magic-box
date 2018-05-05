edf13959ffa34be52e570a644d66a0ce0c121b5a
moumao <1195358569@qq.com>
2018-05-05 16:07:26 +0800

    feat: 完善react react-redux react-router以及webpack的基础配置

--- magic-box-builder/webpack.dev.config.js
+++ magic-box-builder/webpack.dev.config.js
@@ -1,42 +1,26 @@
 const path = require('path');
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
+const merge = require('webpack-merge');
+const commonConfig = require('./webpack.common.config.js');
 
-    /*入口*/
-    entry: {
-        app: path.join(__dirname, 'src/index.js')
-    },
+const devConfig = {
+
+    devtool: 'inline-source-map',
 
     /*输出到dist文件夹，输出文件名字为bundle.js*/
     output: {
-        path: path.join(__dirname, './dist'),
-        filename: 'bundle.js'
+        filename: '[name].[hash].js',
     },
 
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$$/,
-                use: ['babel-loader?cacheDirectory=true'],
-                include: path.join(__dirname, 'src')
-            },
-            {
-            test: /\.(png|svg|jpg|gif|ico)$/,
-                use: [
-                    'file-loader?name=[name].[ext]'
-                ]
-            }
-        ]
-    },
-    plugins: [
-        new webpack.NamedModulesPlugin(),
-        new webpack.HotModuleReplacementPlugin()
-    ],
     devServer: {
         port: 3002,
         contentBase: path.join(__dirname, './dist'),
         historyApiFallback: true,
         hot: true
     }
+
 };
+
+module.exports = merge(commonConfig, devConfig);
